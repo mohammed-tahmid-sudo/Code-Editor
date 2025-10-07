9e12d0279c59317c0cf505788733d09d3664b90e
mohammed-tahmid-sudo <mohammedtahmid.bd@gmail.com>
2025-10-08 00:01:21 +0600

    updates

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,31 @@
 #include <QApplication>
 #include <QMainWindow>
-#include <QTextEdit>
+#include <QMenu>
+#include <QMenuBar>
+#include <QPlainTextEdit>
+#include <qmenu.h>
+#include <qmenubar.h>
+#include <qnamespace.h>
+#include <qtextedit.h>
 
 int main(int argc, char *argv[]) {
-    QApplication app(argc, argv);
-    QMainWindow window;
-    auto *editor = new QTextEdit;
-    window.setCentralWidget(editor);
-    window.resize(800, 600);
-    window.show();
-    return app.exec();
-}
+  QApplication app(argc, argv);
 
+  QMainWindow window;
+
+  QTextEdit *edit = new QTextEdit();
+
+  edit->setPlaceholderText("Type someting damn it!!!!!");
+
+  QMenuBar *menubar = new QMenuBar();
+  QMenu *file = menubar->addMenu("FILE");
+  
+  file->addAction("Save");
+
+  window.setMenuBar(menubar);
+  window.setCentralWidget(edit);
+  window.resize(1100, 800);
+  window.show();
+
+  return app.exec();
+}
